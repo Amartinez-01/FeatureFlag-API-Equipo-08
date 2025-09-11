03b802cb74caa3881d10a03809996a57a95551ba
juanmedina100 <92758289+juanmedina100@users.noreply.github.com>
2025-09-11 14:08:43 -0600

    En Feature el name sea unico y en description no null

--- featureflag-api/src/main/java/com/empresa/featureflag_api/Model/Feature.java
+++ featureflag-api/src/main/java/com/empresa/featureflag_api/Model/Feature.java
@@ -22,9 +22,9 @@ public class Feature {
     @GeneratedValue
     @Column(columnDefinition = "UUID")
     private UUID id;
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 100, unique = true)
     private String name;
-    @Column(length = 500)
+    @Column(length = 500, nullable = false)
     private String description;
     @Column(nullable = false, name = "enabled_by_default")
     Boolean enabledByDefault = false;
